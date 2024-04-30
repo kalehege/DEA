ef668550d7378e8d5071f2923b22ff5a2ef369c2
chamini <chaminimaduwanthi97@gmail.com>
2024-05-01 01:49:48 +0530

    update contact query

--- src/java/com/product/StoreDB.java
+++ src/java/com/product/StoreDB.java
@@ -30,7 +30,7 @@ public class StoreDB {
         " (?, ?, ?, ?, ?);";
     
         
-    private static final String INSERT_Contact_SQL = "INSERT INTO contacts" + "  (name, email, message) VALUES " +
+    private static final String INSERT_Contact_SQL = "INSERT INTO contacts" + "  (name, email, subject, message) VALUES " +
         " (?, ?, ?);";
 
     private static final String SELECT_ALL_Products = "select * from products ORDER BY ID DESC;";
