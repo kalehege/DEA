5ac4f375e27188c2c75c43479fa96a4b78a019c5
hasi <hashanshavinda572@gmail.com>
2024-05-01 01:03:45 +0530

    add getters and setters

--- src/java/com/product/Contact.java
+++ src/java/com/product/Contact.java
@@ -36,5 +36,37 @@ public class Contact {
         this.email = email;
         this.message = message;
      }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
     
 }
